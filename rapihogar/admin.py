015c67c4ae9b845b636769df22ff7ae2896b9f7f
Nicolas <nicolasre011@gmail.com>
2023-03-07 06:25:57 -0300

    Registered `Repairman` on admin site

--- rapihogar/admin.py
+++ rapihogar/admin.py
@@ -1,7 +1,8 @@
-from .models import Company, User, Scheme, Pedido
+from .models import Company, User, Scheme, Pedido, Repairman
 from django.contrib import admin
 
 admin.site.register(Company)
 admin.site.register(User)
 admin.site.register(Scheme)
 admin.site.register(Pedido)
+admin.site.register(Repairman)
