e37eb5b450a99147e94272e7cf95b18ff6b2f867
Nicolas <nicolasre011@gmail.com>
2023-03-07 08:54:01 -0300

    Code optimizations

--- api/services.py
+++ api/services.py
@@ -11,25 +11,29 @@ class RepairmanService:
     @classmethod
     def get_summary(cls):
         """ Returns a summary of all the repairman's """
-        queryset = Repairman.objects.all()
-        average_pay = cls.calculate_average_pay(queryset)
+        repairman_list = cls.get_list()
+
+        average_pay = fmean([r.get('total_pay', None) for r in repairman_list])
+
         data = {
-            'average_pay': average_pay, 'below_average': [],
-            'min_pay': (cls.get_latest_min_and_max_pay(queryset))[0],
-            'max_pay': (cls.get_latest_min_and_max_pay(queryset))[1]
+            'average_pay': average_pay,
+            'below_average': [],
         }
 
-        repairman_list = cls.get_list({})
         for repairman in repairman_list:
             if repairman['total_pay'] < average_pay:
                 data['below_average'].append(repairman)
 
+        sorted_repairman_list = sorted(repairman_list, key=lambda x: x['added_at'], reverse=True)
+        data['min_pay'] = min(sorted_repairman_list, key=lambda x: x['total_pay'])['full_name']
+        data['max_pay'] = max(sorted_repairman_list, key=lambda x: x['total_pay'])['full_name']
+
         return data
 
     @classmethod
-    def get_list(cls, query_params):
+    def get_list(cls, query_params=None):
         """ Returns all the repairman's with fullname, hours worked, total pay and orders """
-        fullname = query_params.get('fullname')
+        fullname = query_params.get('fullname') if query_params else None
         if fullname:
             repairmans = Repairman.objects.filter(Q(first_name__icontains=fullname) | Q(last_name__icontains=fullname))
         else:
@@ -41,7 +45,8 @@ class RepairmanService:
                 'full_name': repairman.full_name,
                 'hours_worked': cls.calculate_hours_worked(repairman),
                 'total_pay': cls.calculate_total_pay(repairman),
-                'orders': Pedido.objects.filter(repairman=repairman).count()
+                'orders': Pedido.objects.filter(repairman=repairman).count(),
+                'added_at': repairman.added_at
             }
             data.append(repairman_data)
 
@@ -60,9 +65,6 @@ class RepairmanService:
         """ Calculates the total pay of a repairman """
         hours_worked = cls.calculate_hours_worked(repairman=repairman)
 
-        if not hours_worked:
-            return 0
-
         payment_rates_table = [
             [15, 200, 0.15],
             [29, 250, 0.16],
@@ -73,26 +75,3 @@ class RepairmanService:
         for payment_rate in payment_rates_table:
             if hours_worked < payment_rate[0]:
                 return (hours_worked * payment_rate[1]) - (hours_worked * payment_rate[1] * payment_rate[2])
-
-    @classmethod
-    def calculate_average_pay(cls, queryset):
-        """ Calculates the average of all the repairman's pay """
-        total_pay_list = []
-        for repairman in queryset:
-            total_pay_list.append(cls.calculate_total_pay(repairman))
-
-        return fmean(total_pay_list)
-
-    @classmethod
-    def get_latest_min_and_max_pay(cls, queryset):
-        """ Get the latest added repairman with max total pay """
-        repairman_list = []
-        for repairman in queryset:
-            data = {'repairman': repairman, 'total_pay': cls.calculate_total_pay(repairman)}
-            repairman_list.append(data)
-
-        sorted_repairman_list = sorted(repairman_list, key=lambda x: x['repairman'].added_at, reverse=True)
-        repairman_with_min_pay = min(sorted_repairman_list, key=lambda x: x['total_pay'])['repairman']
-        repairman_with_max_pay = max(sorted_repairman_list, key=lambda x: x['total_pay'])['repairman']
-
-        return repairman_with_min_pay.full_name, repairman_with_max_pay.full_name
