b4cce28d90cb8dcc3ec3f6bd3f1b4f3a8df4124a
Nicolas <nicolasre011@gmail.com>
2023-03-07 06:59:44 -0300

    Added paths `repairman/` and `repairman/summary/`

--- api/urls.py
+++ api/urls.py
@@ -1,12 +1,13 @@
-from rapihogar.models import Company
 from rest_framework import routers
 from django.urls import path, include
-from .views import CompanyViewSet
+
+from api.views import CompanyViewSet, RepairmanList, RepairmanSummary
 
 router = routers.DefaultRouter()
 router.register(r'company', CompanyViewSet, basename='company')
 
-
 urlpatterns = [
     path('', include(router.urls)),
+    path('repairman/', RepairmanList.as_view(), name="repairman"),
+    path('repairman/summary/', RepairmanSummary.as_view(), name="repairman-summary"),
 ]
